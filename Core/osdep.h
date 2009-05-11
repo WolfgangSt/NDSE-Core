65e4544dbf9bbf6b329b24b45386776cb5da184f
WolfgangSt <WolfgangSt@spambog.de>
2009-05-11 12:13:04 +0200

    includes for nix

--- Core/osdep.h
+++ Core/osdep.h
@@ -6,7 +6,9 @@
 #ifdef WIN32
 #include "signal2/winsig.h"
 #else
-
+#include <ucontext.h>
+#include <sys/mman.h>
+#include <asm/cachectl.h>
 #endif
 
 // signal2 sets a signal handler for sig but only on the calling thread
