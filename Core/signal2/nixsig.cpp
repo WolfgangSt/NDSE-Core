2461d3ee5bfacca665e8c7d3b537b4c634baf251
user <user@ubuntu904desktop.localdomain>
2009-05-14 01:39:59 -0400

    nixsig additions

--- Core/signal2/nixsig.cpp
+++ Core/signal2/nixsig.cpp
@@ -1,6 +1,10 @@
-#include "nixsig2.h"
+#include "nixsig.h"
+#include <signal.h>
 
 void signal2(int sig, sigaction_t handler)
 {
+	struct sigaction sa;
+	struct sigaction old;
+	sigaction( sig, &sa, &old );
 }
 
