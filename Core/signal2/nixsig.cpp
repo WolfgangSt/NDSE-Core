1d45397db5c19272f57a78c162de8b9ef350a530
user <user@ubuntu904desktop.localdomain>
2009-05-14 08:25:13 -0400

    Cleanups and fixes

--- Core/signal2/nixsig.cpp
+++ Core/signal2/nixsig.cpp
@@ -36,34 +36,30 @@ static Qt::HANDLE current_thread()
 
 static void global_handler(int sig, siginfo_t *info, void *ctx)
 {
-	std::cout << "Signal handler for " << sig
-		<< " on " << current_thread() << std::endl;
 	SigMap::const_iterator it = sigs.find(current_thread());
 	if (it != sigs.end())
 	{
 		// do the callback if specified
 		sigaction_t cb = it->second.sig[sig];
 		if (cb)
-		{
-			std::cout << "Calling back to " << cb << std::endl;
-			cb(sig, info, ctx);
-		}
+			return cb(sig, info, ctx);
 	}
+	// invoke the original handler
+	original[sig].original
 }
 
 void signal2(int sig, sigaction_t handler)
 {
-	struct sigaction sa;
-	sa.sa_sigaction = global_handler;
 	sigs[current_thread()].sig[sig] = handler;
-
-	std::cout << "Installing signal handler for " << sig
-		<< " on " << current_thread() << std::endl;
-
 	original_action &oa = original[sig];
 	if (!oa.override)
 	{
 		oa.override = true;
+		struct sigaction sa;
+		sa.sa_sigaction = global_handler;
+		sa.sa_flags = SA_SIGINFO;
+		sigemptyset(&sa.sa_mask);
+		sigaddset(&sa.sa_mask, sig);
 		sigaction( sig, &sa, &oa.original );
 	}
 }
